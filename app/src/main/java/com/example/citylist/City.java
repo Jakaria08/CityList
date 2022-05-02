ac6d0c4fff85092fb6e68cceb45877253320b25c
jakaria08 <jakaria.rabbi@live.com>
2022-05-02 17:36:29 +0600

    working

---
+++ app/src/main/java/com/example/citylist/City.java
@@ -0,0 +1,19 @@
+package com.example.citylist;
+
+public class City {
+    private String city;
+    private String province;
+
+    City(String city, String province){
+        this.city = city;
+        this.province = province;
+    }
+
+    String getCityName(){
+        return this.city;
+    }
+
+    String getProvinceName(){
+        return this.province;
+    }
+}
