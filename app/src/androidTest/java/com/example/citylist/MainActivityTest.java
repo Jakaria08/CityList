35baec3f477050d9857efed1edad12163ed3b659
jakaria08 <jakaria.rabbi@live.com>
2022-05-21 00:39:37 +0600

    running ui app with running ui tests - ok for the lab-3

--- app/src/androidTest/java/com/example/citylist/MainActivityTest.java
+++ app/src/androidTest/java/com/example/citylist/MainActivityTest.java
@@ -1,10 +1,19 @@
 package com.example.citylist;
 
+import static androidx.test.espresso.Espresso.onData;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.action.ViewActions.typeTextIntoFocusedView;
+import static androidx.test.espresso.assertion.ViewAssertions.doesNotExist;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
 import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
 import static androidx.test.espresso.Espresso.onView;
 
+import static org.hamcrest.CoreMatchers.anything;
+import static org.hamcrest.CoreMatchers.not;
+
+import androidx.test.espresso.action.ViewActions;
 import androidx.test.ext.junit.rules.ActivityScenarioRule;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 import androidx.test.filters.LargeTest;
@@ -22,7 +31,40 @@ public class MainActivityTest {
             new ActivityScenarioRule<>(MainActivity.class);
 
     @Test
-    public void listGoesOverTheFold() {
-        onView(withText("CityList")).check(matches(isDisplayed()));
+    public void testAppName() {
+        onView(withText("CityList")).check(matches(isDisplayed())); //Check the name on the screen
     }
+
+    @Test
+    public void testAddCity(){
+        onView(withId(R.id.button_add)).perform(click()); //Click add button to add a city to the list
+        onView(withId(R.id.editText_name)).perform(ViewActions.typeText("Edmonton")); //Type a city name
+        onView(withId(R.id.button_confirm)).perform(click()); //Confirm the city name and add to the list
+        onView(withText("Edmonton")).check(matches(isDisplayed())); //Check the name on the screen
+    }
+
+    @Test
+    public void testClearCity(){
+        onView(withId(R.id.button_add)).perform(click()); //Click add button to add a city to the list
+        onView(withId(R.id.editText_name)).perform(ViewActions.typeText("Edmonton")); //Type a city name
+        onView(withId(R.id.button_confirm)).perform(click()); //Confirm the city name and add to the list
+        //Add another city to the list
+        onView(withId(R.id.button_add)).perform(click()); //Click add button to add a city to the list
+        onView(withId(R.id.editText_name)).perform(ViewActions.typeText("Toronto")); //Type a city name
+        onView(withId(R.id.button_confirm)).perform(click()); //Confirm the city name and add to the list
+        //Clear the list
+        onView(withId(R.id.button_clear)).perform(click());
+        onView(withText("Edmonton")).check(doesNotExist());
+
+    }
+    @Test
+    public void testListView(){
+        onView(withId(R.id.button_add)).perform(click()); //Click add button to add a city to the list
+        onView(withId(R.id.editText_name)).perform(ViewActions.typeText("Edmonton")); //Type a city name
+        onView(withId(R.id.button_confirm)).perform(click()); //Confirm the city name and add to the list
+
+        onData(anything()).inAdapterView(withId(R.id.city_list)).atPosition(0).
+                check(matches((withText("Edmonton")))); //Check the content on the list - no content in this case
+    }
+    
 }
