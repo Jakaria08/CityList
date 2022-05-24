549dc8828af1ce9eac347a5230b5eab0a53b7176
Shoumya <shoummochoudhury@yahoo.com>
2022-05-24 19:58:24 +0600

    Final Commit

--- app/src/main/java/com/example/citylist/CityList.java
+++ app/src/main/java/com/example/citylist/CityList.java
@@ -2,6 +2,7 @@ package com.example.citylist;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 /**
@@ -15,7 +16,7 @@ public class CityList {
      * @param city
      *      This is the city to add
      */
-    public void add(City city) {
+    public void addCity(City city) {
         if (cities.contains(city)) {
             throw new IllegalArgumentException();
         }
@@ -23,13 +24,47 @@ public class CityList {
     }
 
     /**
+     * This method deletes a city from cityList.If the given city is absent in cityList then it also throws and exception.
+     * @param givenCity
+     *      The city to be deleted.
+     */
+
+    public void deleteCity(City givenCity){
+        if(!cities.contains(givenCity))
+            throw new IllegalArgumentException();
+
+        else
+            cities.remove(givenCity);
+    }
+
+    /**
+     * This method returns total available city at the current moment in cityList.
+     * @return
+     */
+
+    public int getCityCount(){
+        return cities.size();
+    }
+
+    /**
      * This returns a sorted list of cities
      * @return
      *      Return the sorted list of cities
      */
-    public List<City> getCities() {
+    public List<City> getCities(int parity) {
         List<City> cityList = cities;
-        Collections.sort(cityList);
+
+        if(parity==1)
+            Collections.sort(cityList);
+
+        else
+            Collections.sort(cityList, new Comparator<City>() {
+                @Override
+                public int compare(City o1, City o2) {
+                        return o1.getProvinceName().compareTo(o2.getProvinceName());
+                }
+            });
+
         return cityList;
     }
 }
