7eacb14a72957ffefdc124a00259ba056824f660
Sk-Azraf-Sami <azraf7026official@gmail.com>
2023-05-05 22:55:05 +0600

    Doing UI Test

---
+++ app/src/androidTest/java/com/example/citylist/ShowActivityTest.java
@@ -0,0 +1,12 @@
+package com.example.citylist;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+
+public class ShowActivityTest {
+
+}
